27706e365a50dd62e0beeea317f6efeaa6ec3927
GaneshPawar26 <146693575+GaneshPawar26@users.noreply.github.com>
2026-07-08 19:40:15 +0000

    2_datatypes

---
+++ 01_Basics/2_datatypes.js
@@ -0,0 +1,8 @@
+"use strict";//test js as new version
+
+
+let status=null;
+let age;
+
+console.log(typeof(age));
+console.log(typeof(status));
--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,2 +1,9 @@
 console.log("Ganesh");
 console.log("Pawar");
+
+
+let name="Refrigerator"
+const price=42000;
+var company="Samsung";
+
+console.table([name,price,company]);
