a36c324959a4aac24f80bf1dfeef8eee45337a3f
GaneshPawar26 <146693575+GaneshPawar26@users.noreply.github.com>
2026-07-11 19:13:19 +0000

    ***************

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -8,3 +8,22 @@ console.log(typeof(score));
 console.log(typeof(Number(score)));
 console.log(score)
 
+console.log(1+Number("2"));
+
+console.log("2"===2)
+console.log("2"==2)
+
+
+function add(a,b)
+{
+    return a+b;
+}
+
+console.log(add(99,11));
+
+let avg=function(a,b,c,d)
+{
+    return (a+b+c+d)/4;
+}
+
+console.log(avg(3,5,6,7))
